2bb21b622a4e38ba0552e42c2e997061ffba438a
KarpovDenis74 <fotokarpov@yandex.ru>
2020-08-16 18:49:30 +0300

    pytest: OK1

--- homework.py
+++ homework.py
@@ -12,12 +12,14 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 TIME_OUT = int(os.getenv('TIME_OUT'))
 PRACTICUM_SERVER = os.getenv('PRACTICUM_SERVER')
 
+
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    if homework.get('status')  == 'rejected':
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = ('Ревьюеру всё понравилось, '
+            'можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -30,7 +32,8 @@ def get_homework_statuses(current_timestamp):
                         headers=headers,
                         params=params)
     log_get_homework_statuses = logging.getLogger("Log1.get_homework_statuses")
-    log_get_homework_statuses.info(f'Запрос статуса домашки. Ответ {PRACTICUM_SERVER} - {homework_statuses.status_code}')                    
+    log_get_homework_statuses.info('Запрос статуса домашки. Ответ '
+        f'{PRACTICUM_SERVER} - {homework_statuses.status_code}')
     return homework_statuses.json()
 
 
@@ -49,17 +52,20 @@ def main():
     logger = logging.getLogger("Log1")
     logger.setLevel(logging.INFO)
     fh = logging.FileHandler(filename="log.log", mode="w")
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s'
+        ' - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     logger.addHandler(fh)
-    logger.info("Программа запущена") 
+    logger.info("Программа запущена")
     current_timestamp = int(time.time())
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 logger.info("Статус домашки изменился")
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+                send_message(parse_homework_status(
+                    new_homework.get(
+                        'homeworks')[0]))
             current_timestamp = new_homework.get('current_date')
             time.sleep(TIME_OUT)
             logger.info("Очередной запрос по циклу while статуса домашки")
